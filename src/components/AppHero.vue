fb68c948a1ee64b0ed6b481063097708cabdc4e6
Cris-NTL <natolicristian92@gmail.com>
2023-04-13 19:43:30 +0200

    Add: title + jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+            
+        };
+    },
+};
+</script>
+
+<template>
+    <img src="../assets/img/tree.jpg" alt="jumboImage" />
+</template>
